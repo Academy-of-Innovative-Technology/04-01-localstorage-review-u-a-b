3e6c692aa75a45df090b8d8a1ae4b9ebffd6c939
u-a-b <uamayabanegas26@aoiths.org>
2026-02-04 13:55:16 -0500

    Complete

--- script.js
+++ script.js
@@ -6,23 +6,28 @@ document.querySelector(".save").onclick = function() {
 let saveValue = document.querySelector("#saveValue").value;
 localStorage.setItem(key, saveValue);
 let saveKey = document.querySelector("#saveKey");
-saveKey.clear();
+let saveValueClear = document.querySelector("#saveValue");
+saveValueClear.value = "";
+saveKey.value = "";
 };
 
 
 // LOAD
 document.querySelector(".load").onclick = function() {
     let data = localStorage.getItem(key);
+    console.log(data);
    let loadValue = document.querySelector("#loadValue");
-   loadValue.innerHTML = data;
+   loadValue.value = data;
 };
 
 // LOAD
 document.querySelector(".update").onclick = function() {
-
+let updateValue = document.querySelector("#updateValue").value;
+let data = localStorage.setItem(key, updateValue);
 };
 
 // DELETE
 document.querySelector(".delete").onclick = function() {
-
+let deleteKey = document.querySelector("#deleteKey").value;
+localStorage.removeItem(deleteKey);
 };
